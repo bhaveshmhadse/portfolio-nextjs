30f2953b28ffe2979a0111b4a9485e1dca7a372d
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2022-02-13 14:54:55 +0530

    Refactored

--- components/Journey/JourneySection.tsx
+++ components/Journey/JourneySection.tsx
@@ -3,7 +3,7 @@ import LayoutFlow from "./Flow";
 const Journey = () => {
   return (
     <div>
-      J<div className='header'>My Journey</div>
+      <div className='header'>My Journey</div>
       <LayoutFlow />
     </div>
   );
