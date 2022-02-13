e8954e3442b0848eafcfdd5d7bb77c72979c1452
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2022-02-13 14:52:06 +0530

    Refactored

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from "next";
+import { useEffect } from "react";
 
 import HeroSection from "../components/Hero/HeroSection";
-import LayoutFlow from "../components/Journey/Flow";
+// import LayoutFlow from "../components/Journey/Flow";
 import Journey from "../components/Journey/JourneySection";
 import ProjectsSection from "../components/Projects/ProjectsSection";
 import SkillsSection from "../components/Skills/SkillsSection";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className='w-full bg-zinc-800 text-white '>
       <HeroSection />
