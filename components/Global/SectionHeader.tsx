707ad29b7472212365cb3fc250df816ac90e8648
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2022-02-13 15:17:00 +0530

    Refactored

--- components/Global/SectionHeader.tsx
+++ components/Global/SectionHeader.tsx
@@ -1,5 +1,5 @@
 const SectionHeader = ({ title }: { title: string }) => {
-  return <div className="font-extrabold uppercase lg:text-4xl text-3xl -translate-y-10 lg:text-center text-left px-6 -mb-10 lg:mb-0">{title}</div>;
+  return <div className='font-extrabold uppercase lg:text-6xl text-5xl -translate-y-10 lg:text-center text-left px-6 -mb-10 lg:mb-0'>{title}</div>;
 };
 
 export default SectionHeader;
