0cf9c6e63859e34797d41c9282cb505170262d8c
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2022-07-10 21:01:57 +0530

    Refactored

--- components/Skills/Skill.tsx
+++ components/Skills/Skill.tsx
@@ -1,5 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+
 import { isEven } from "../../controllers/helperFuntions";
 
 const Skill = ({ title, index }: any) => {
--- components/Skills/SkillSet.tsx
+++ components/Skills/SkillSet.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
-import { Skills } from "../../controllers/getSkills";
+import { useState } from "react";
+import { Skills } from "./SkillData";
 import SkillsContainer from "./SkillsContainer";
 
 const SkillSet = () => {
