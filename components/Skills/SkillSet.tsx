83da7b5d58741fa00f08cf93d5dfb16b666539a7
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2022-07-10 19:41:46 +0530

    Refactored

--- components/Skills/SkillSet.tsx
+++ components/Skills/SkillSet.tsx
@@ -9,14 +9,14 @@ const SkillSet = () => {
   return (
     <div className='w-full block text-center  h-auto'>
       <div className='flex w-full items-center justify-center text-center   '>
-        <div className='lg:w-1/2 lg:p-2 text-center flex w-full justify-center'>
-          <motion.button className={`lg:text-xl text-sm rounded px-6 py-4  font-bold gr duration-500 ease-in-out ${selectedSkill == "Languages" ? " green-border green-text" : " text-gray-600"} border-4 border-transparent transition-all ease-in-out duration-300 paragraph`} onClick={() => setselectedSkill("Languages")}>
+        <div className='lg:w-1/2 lg:p-2 px-8 text-center flex w-full justify-center '>
+          <motion.button className={`lg:text-xl text-sm rounded lg:px-6 px-4 py-4  font-bold gr duration-500 ease-in-out ${selectedSkill == "Languages" ? " green-border green-text" : " text-gray-600"} border-4 border-transparent transition-all ease-in-out duration-300 paragraph`} onClick={() => setselectedSkill("Languages")}>
             Languages
           </motion.button>
-          <motion.button className={`lg:text-xl text-sm rounded px-6 py-4  font-bold gr duration-500 ease-in-out ${selectedSkill == "Frameworks" ? " green-border green-text" : " text-gray-600"} border-4 border-transparent transition-all ease-in-out duration-300 paragraph`} onClick={() => setselectedSkill("Frameworks")}>
+          <motion.button className={`lg:text-xl text-sm rounded lg:px-6 px-4 py-4  font-bold gr duration-500 ease-in-out ${selectedSkill == "Frameworks" ? " green-border green-text" : " text-gray-600"} border-4 border-transparent transition-all ease-in-out duration-300 paragraph`} onClick={() => setselectedSkill("Frameworks")}>
             Frameworks
           </motion.button>
-          <motion.button className={`lg:text-xl text-sm rounded px-6 py-4  font-bold gr duration-500 ease-in-out ${selectedSkill == "Databases" ? " green-border green-text" : " text-gray-600"} border-4 border-transparent transition-all ease-in-out duration-300 paragraph`} onClick={() => setselectedSkill("Databases")}>
+          <motion.button className={`lg:text-xl text-sm rounded lg:px-6 px-4 py-4  font-bold gr duration-500 ease-in-out ${selectedSkill == "Databases" ? " green-border green-text" : " text-gray-600"} border-4 border-transparent transition-all ease-in-out duration-300 paragraph`} onClick={() => setselectedSkill("Databases")}>
             Databases
           </motion.button>
         </div>
--- components/Skills/SkillsContainer.tsx
+++ components/Skills/SkillsContainer.tsx
@@ -17,11 +17,11 @@ const SkillsContainer = ({ arrayOfSkills }) => {
     <div className='flex items-center pt-11 mb-8 justify-center lg:mt-auto h-auto lg:w-full lg:h-screen '>
       <div className=' px-4 block lg:flex items-center justify-center'>
         {/* <ProjectImageMain projectName={projectName} /> */}
-        <div className='flex w-full  px-6 items-center justify-center'>
+        <div className='flex w-full items-center justify-center'>
           <div className='w-full h-auto lg:h-1/4 grid grid-cols-2 lg:grid-cols-3 lg:gap-x-0 lg:gap-y-0 gap-x-2 gap-y-8'>
             {arrayOfSkills.map(eachObj => (
               <div className='lg:p-0 w-full h-full lg:py-2 p-2 flex flex-col items-center justify-center' key={Math.random().toString()}>
-                <img className='lg:w-5/6 w-full zoomerTwo rounded-lg drop-shadow-lg' src={"projects/pathfinding visualizer.jpg"} alt='Image' />
+                <img className='lg:w-5/6 w-full zoomerTwo rounded-lg drop-shadow-lg' src={`skills/${eachObj.title}.png`} alt='Image' />
                 <span className='pt-6 select-none  font-black text-zinc-400 uppercase whitespace-nowrap'>{eachObj.title}</span>
               </div>
             ))}
